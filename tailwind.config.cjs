6534d4f347f5250e4215eb340f91450dcc003633
doctorgde1 <rakov5377357536277hfhsghjsgh@gmail.com>
2023-03-17 16:24:15 +0200

    build: added theme colors to the tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "midnight-express": {
+          100: "#E4F2FC",
+          300: "#B6DCF6",
+          500: "#12629B",
+          700: "#00396C",
+          900: "#001341",
+        },
+      },
   },
   plugins: [],
 };
